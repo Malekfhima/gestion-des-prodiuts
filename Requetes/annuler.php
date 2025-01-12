b8869bc1f936437784e9bcfdb1ccc0a0f4b7011d
Malek Fhima <malekfhima1@gmail.com>
2025-01-12 12:13:47 +0100

    first commit

---
+++ Requetes/annuler.php
@@ -0,0 +1,8 @@
+<?php
+session_start();
+include("../connect.php");
+$id = $_POST['id'];
+$requete =("DELETE FROM 'vente' WHERE id =?");
+$res=$cnx->query($requete);
+header('Location: ../index.php');
+?>
