1fb04232375cacd3015777745a9dcdd393afaa67
Malek Fhima <malekfhima1@gmail.com>
2025-01-30 16:50:37 +0100

    malek

--- inscrit/main.php
+++ inscrit/main.php
@@ -1,10 +1,15 @@
 <?php
+include("../connect.php");
 extract($_POST);
-$cnx = mysqli_connect("127.0.0.1","root","","ifbreak");
-$req5 = "insert into personne (passe,nom,mail,monnaire) values('$mp','$nom','$email','$flous');" or die("probelm l4") ;
-$res9 = mysqli_query($cnx,$req5) or die("problim ins");
-$nb5 = mysqli_affected_rows($cnx);
-sleep(1);
+session_start();
+$res=mysqli_query($cnx,"SELECT * FROM personne where nom='$nom'");
+$nb=mysqli_num_rows($res);
+if($nb>0){
+    echo"<script>alert('le compte est deja exeicete');</script>";
+}else{
+    $res=mysqli_query($cnx,"insert into personne (passe,nom,mail,monnaire) values('$mp','$nom','$email','$flous');");
+    sleep(1);
 header("location:../connexion/index.html");
 mysqli_close($cnx);
+}
 ?>
